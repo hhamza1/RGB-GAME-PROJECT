81cb343d991c82f8cb1a9f94217ced97e9254614
hhamza1 <hamzahmitou@gmail.com>
2018-07-26 20:30:59 +0300

    Define a function to set colors

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 var squares = document.querySelectorAll(".square");
 var selectedColor =  document.querySelector("#selectedColor");
+var resetGame = document.querySelector("#new-game");
 var colors = new Array();
+var pickedColor;
+
+setColors();
 
 
-for(var i= 0; i < 6; i++) {
-  colors [i] = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
-  console.log(colors[i]);
+function setColors() {
+  for(var i= 0; i < 6; i++) {
+    colors [i] = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+  }
+  pickedColor = colors[Math.floor(Math.random() * 6)]
+  selectedColor.textContent = pickedColor;
 }
-
-var pickedColor = colors[Math.floor(Math.random() * 6)];
-selectedColor.textContent = pickedColor;
-
-
 
 for (var i = 0; i < squares.length; i++ ) {
   squares[i].style.background = colors[i];
 }
+
+
+resetGame.addEventListener("click", function(){
+  setColors;
+});
