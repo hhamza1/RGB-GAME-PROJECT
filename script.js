235d2a5d553d4c35eb0008f7898f246d9f9694c3
hhamza1 <hamzahmitou@gmail.com>
2018-07-27 03:43:55 +0300

    Fix Reset button function

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
 var squares = document.querySelectorAll(".square");
+var topHeader = document.getElementById("top-header");
 var selectedColor =  document.querySelector("#selectedColor");
 var resetGame = document.querySelector("#new-game");
-var colors = new Array();
+var messageDisplay = document.querySelector("#try-again");
+var colors = setColors(6);
 var pickedColor;
 
-function setColors() {
-  for(var i= 0; i < 6; i++) {
-    colors [i] = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
+function setColors(num) {
+  var colorArray = [];
+  for(var i= 0; i < num; i++) {
+    colorArray.push(randomColors());
+  }
+  return colorArray;
+}
+
+function randomColors() {
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  return "rgb("+ red +", "+ green +", "+ blue +")";
+}
+
+
+  function winningColor(clickedColor) {
+    for(var i= 0; i < 6; i++) {
+      squares[i].style.backgroundColor = clickedColor;
+    }
   }
 
   for (var i = 0; i < squares.length; i++ ) {
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click", function(){
-      if(this.style.backgroundColor === pickedColor){
-        console.log("true");
+      var clickedColor = this.style.backgroundColor;
+      if( clickedColor === pickedColor){
+          messageDisplay.textContent = "Correct";
+          topHeader.style.backgroundColor = pickedColor;
+          resetGame.textContent = "Play Again?";
+          winningColor(clickedColor);
+
       }
       else {
-        console.log("false");
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
       }
     })
   }
 
-  pickedColor = colors[Math.floor(Math.random() * 6)]
-  selectedColor.textContent = pickedColor;
+function pickColor() {
+  pickedColor = colors[Math.floor(Math.random() * colors.length)]
+  return pickedColor
 }
 
-
-document.addEventListener("DOMContentLoaded", function(event){
-  setColors();
-})
+selectedColor.textContent = pickColor();
 
 
 resetGame.addEventListener("click", function(){
-  setColors();
+    resetGame.textContent = "New Colors?";
+   colors = setColors(6);
+   pickColor();
+   for(var i=0; i< squares.length; i++){
+     squares[i].style.background = colors[i];
+   }
 });
