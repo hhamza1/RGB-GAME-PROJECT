2e209017cc38b5b1f6ec641e3fd9e6910cda17be
hhamza1 <hamzahmitou@gmail.com>
2018-07-27 05:21:44 +0300

    Clean and Refactore code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var squares = document.querySelectorAll(".square");
+var mods = document.querySelectorAll(".mod");
 var squaresNum = 6;
 var topHeader = document.querySelector("h1");
 var selectedColor =  document.querySelector("#selectedColor");
@@ -28,51 +29,24 @@ for (var i = 0; i < squares.length; i++ ) {
     })
   }
 
-  function setColors(num) {
-    var colorArray = [];
-    for(var i= 0; i < num; i++) {
-      colorArray.push(randomColors());
-    }
-    return colorArray;
+for(var i=0; i< mods.length; i++){
+    mods[i].addEventListener("click", function(){
+        mods[0].classList.remove("selected");
+        mods[1].classList.remove("selected");
+        this.classList.add("selected");
+        this.textContent === "Easy" ? squaresNum = 3 : squaresNum = 6;
+        reset();
+      });
   }
-
-  function randomColors() {
-    var red = Math.floor(Math.random() * 256);
-    var green = Math.floor(Math.random() * 256);
-    var blue = Math.floor(Math.random() * 256);
-    return "rgb("+ red +", "+ green +", "+ blue +")";
-  }
-
-
-    function winningColor(clickedColor) {
-      for(var i= 0; i < 6; i++) {
-        squares[i].style.backgroundColor = clickedColor;
-      }
-    }
-
-function pickColor() {
-  pickedColor = colors[Math.floor(Math.random() * colors.length)]
-  return pickedColor
-}
-
-
 
 
 resetGame.addEventListener("click", function(){
-   resetGame.textContent = "New Colors?";
-   topHeader.style.backgroundColor = "#9898FF";
-   messageDisplay.textContent = "";
-   colors = setColors(squaresNum);
-   pickColor();
-   for(var i=0; i< squares.length; i++){
-     squares[i].style.background = colors[i];
-   }
+   reset();
 });
 
-easyMod.addEventListener("click",function() {
-  easyMod.classList.add("selected");
-  hardMod.classList.remove("selected");
-  squaresNum = 3;
+
+function reset() {
+
   colors = setColors(squaresNum);
   selectedColor.textContent = pickColor();
   topHeader.style.backgroundColor = "#9898FF";
@@ -80,29 +54,41 @@ easyMod.addEventListener("click",function() {
   resetGame.textContent = "New Colors?";
 
   for (var i=0; i<squares.length; i++){
-      if(colors[i]) {
-        squares[i].style.backgroundColor = colors[i]
-      }
-      else {
-        squares[i].style.display = "none";
-      }
-  }
-});
-
-hardMod.addEventListener("click",function() {
-  hardMod.classList.add("selected");
-  easyMod.classList.remove("selected");
-  squaresNum = 6;
-  colors = setColors(squaresNum);
-  selectedColor.textContent = pickColor();
-  topHeader.style.backgroundColor = "#9898FF";
-  messageDisplay.textContent = "";
-  resetGame.textContent = "New Colors?";
-
-
-  for (var i=0; i<squares.length; i++){
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "block";
+    if(colors[i]){
+      squares[i].style.display = "block";
+      squares[i].style.backgroundColor = colors[i];
+    }
+    else {
+      squares[i].style.display = "none";
+    }
 
   }
-});
+
+}
+
+function setColors(num) {
+  var colorArray = [];
+  for(var i= 0; i < num; i++) {
+    colorArray.push(randomColors());
+  }
+  return colorArray;
+}
+
+function randomColors() {
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  return "rgb("+ red +", "+ green +", "+ blue +")";
+}
+
+
+  function winningColor(clickedColor) {
+    for(var i= 0; i < 6; i++) {
+      squares[i].style.backgroundColor = clickedColor;
+    }
+  }
+
+function pickColor() {
+pickedColor = colors[Math.floor(Math.random() * colors.length)]
+return pickedColor
+}
